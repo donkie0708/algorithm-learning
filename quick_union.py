fb68e6f725f7ac9569b618e754ce68c974d8b7f1
donkie0708 <tubao1106@gmail.com>
2017-04-11 14:46:20 +0900

    add weighted feature to make sure link smaller tree to larger one when doing union operation

--- quick_union.py
+++ quick_union.py
@@ -1,7 +1,8 @@
-class QuickUnion:
+class WeightedQuickUnion:
   
   def __init__(self, n):
     self.table = list(range(n))
+    self.sz = [1] * n 
 
   def _findRoot(self, index):
     while self.table[index] != index:
@@ -9,18 +10,28 @@ class QuickUnion:
     return index 
 
   def union(self, a, b):
-    self.table[self._findRoot(a)] = self._findRoot(b)
-    
+    i = self._findRoot(a)
+    j = self._findRoot(b)
+    if i == j: return
+    if self.sz[j] >= self.sz[i]:
+      self.table[i] = j
+      self.sz[j] += self.sz[i]
+    else:
+      self.table[j] = i
+      self.sz[i] += self.sz[j]
+      
   def connected(self, a, b):
     return self._findRoot(a) == self._findRoot(b)
     
 
-
-test = QuickUnion(10)
+test = WeightedQuickUnion(10)
 test.union(2,5)
 test.union(5,7)
 test.union(2,3)
 test.union(1,6)
+test.union(8,6)
+test.union(8,7)
 print(test.connected(2,7))
 print(test.connected(1,2))
 print(test.table)
+print(test.sz)
